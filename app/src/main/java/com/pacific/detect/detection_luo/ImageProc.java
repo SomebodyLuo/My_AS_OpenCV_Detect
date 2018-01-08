7e877637c936e918b1565b3916faf148f175c497
SomebodyLuo <luoyouren26@163.com>
2018-01-08 17:09:10 +0800

    Gray Process;

---
+++ app/src/main/java/com/pacific/detect/detection_luo/ImageProc.java
@@ -0,0 +1,56 @@
+package com.pacific.detect.detection_luo;
+
+import android.support.v7.appcompat.R.integer;
+
+public class ImageProc
+{
+	static
+	{
+		System.loadLibrary("image_proc");
+	}
+	
+	public static native int[] grayProc(int[] pixels, int w, int h);
+	
+	public static native int connectCamera(int device);
+	
+	public static native FrameInfo getFrame();	
+	
+	public static native int releaseCamera();
+	
+	public static native int startRecord(String dateString);
+	
+	public static native int stopRecord();
+	
+	public static native int getWidth();
+	
+	public static native int getHeight();
+	
+	public static class FrameInfo
+	{
+		private int mWidth;			//private: int����
+		private int mHeight;			//private: int����
+		private int mImageSize;		
+		private int[] mPixels;		//private: int������������
+		
+		public int getWidth()
+		{
+			return mWidth;
+		}
+		
+		public int getHeigth()
+		{
+			return mHeight;
+		}
+		
+		public int getImageSize()
+		{
+			return mImageSize;
+		}
+		
+		public int[] getPixels()
+		{
+			return mPixels;
+		}		
+	}
+}
+
---
+++ app/src/main/java/com/pacific/detect/detection_luo/callback.java
@@ -0,0 +1,6 @@
+package com.pacific.detect.detection_luo;
+
+public interface callback
+{
+	void setViewText(int id, String str);
+}
